4e50bce3957520daa2174c540fdbf95dd6ba7641
Aymaru <aycastillo@ic-itcr.ac.cr>
2019-11-24 21:17:32 -0600

    Gitignore Agregado

--- tcp_cliente.c
+++ tcp_cliente.c
@@ -6,6 +6,7 @@
 
 int main(){
 	int network_socket;
+	int rc;
 	network_socket = socket(AF_INET,SOCK_STREAM,0);
 	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
@@ -14,7 +15,7 @@ int main(){
 	int connection = connect(network_socket,(struct sockaddr * ) &server_address, sizeof(server_address));
 	char server_response[256];
 	recv(network_socket, server_response, sizeof(server_response),0);
-	printf(server_response);
-	close(network_socket);
+	printf("%s",server_response);
+	rc = close(network_socket);
 	return 0;
 }
