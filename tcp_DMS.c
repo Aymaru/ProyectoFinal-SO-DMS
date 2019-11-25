932ecf601a2538ce77eb668d3802fcce18f6fb05
Unknown <unknown@example.com>
2019-11-24 20:57:20 -0600

    Sockets

---
+++ tcp_DMS.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(){
+	char server_message[256]="Hola Mundo";
+        int server_socket;
+        server_socket = socket(AF_INET,SOCK_STREAM,0);
+        struct sockaddr_in server_address;
+        server_address.sin_family = AF_INET;
+        server_address.sin_port = htons(9001);
+        server_address.sin_addr.s_addr = INADDR_ANY;
+
+        int connection = bind(server_socket,(struct sockaddr * ) &server_address, sizeof(server_address));
+       	listen(server_socket,1);
+	int cliente_socket;
+	cliente_socket = accept(server_socket,NULL,NULL);
+	send(cliente_socket,server_message,sizeof(server_message),0);
+	close(server_socket);
+        return 0;
+}
+
